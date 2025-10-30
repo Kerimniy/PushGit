4fcef1e6c4e6ac26b434056a4bbedf7e53ae797c
Kerimniy <mutalupkerim@gmail.com>
2025-10-30 08:32:48 +0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,11 +1,51 @@
 
 fn main() {
 
+    //compile .slint file
     slint_build::compile("ui\\main.slint").unwrap();
 
+    // install icon (Windows)
     let mut res = winres::WindowsResource::new();
     res.set_icon("ui/pushgit2x32.ico");
     res.compile().unwrap();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
